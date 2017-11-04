1caa4a05a182e041afd8895b8f812d9bdc617c32
trigtop <tustxk@163.com>
2017-11-04 06:36:18 +0000

    can_verify = False

---
+++ xk_config.sh
@@ -0,0 +1,11 @@
+#########################################################################
+# File Name: xk_config.sh
+# Author: ke.xiao
+# mail: ke.xiao@netxeon.com
+# Created Time: Tue 12 Sep 2017 08:32:53 PM PDT
+#########################################################################
+#!/bin/bash
+
+mkdir -p ~/bin
+cp repo ~/bin/repo
+chmod a+x ~/bin/repo
